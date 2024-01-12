c8de9bd9a156924b8ed3b9f785ae6325d66e9055
Airly12000 <littlefrancis28@gmail.com>
2024-01-12 02:16:14 +0000

    parameter change

--- database/Connection.js
+++ database/Connection.js
@@ -3,8 +3,8 @@ const Sequelize = require("sequelize");
 require("dotenv").config({ path: path.join(__dirname, "../", ".env") });
 
 module.exports = new Sequelize(
-  process.env.DB_USERNAME,
   process.env.DB_DATABASE,
+  process.env.DB_USERNAME,
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
