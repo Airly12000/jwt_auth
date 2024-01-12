b77be86dee0427b0a453b8cbfc7c559987fc3f67
Airly12000 <littlefrancis28@gmail.com>
2024-01-12 02:18:17 +0000

    register, login, logout

--- routes/Users.js
+++ routes/Users.js
@@ -1,9 +1,61 @@
 const express = require("express");
+const bcrypt = require("bcrypt");
+const { genJWT, deleteJWT } = require("../auth/Auth");
+const Users = require("../models/Users");
 
 const router = express.Router();
 
+const errorHandler = (error) => console.log(`Error: ${error}`);
+
 router.get("/", (req, res) => {
-  res.json({ message: "Hello User" });
+  res.json({ message: "Hello" });
 });
+
+router.post("/register", genJWT, async (req, res) => {
+  const body = req.body;
+  const encrypted_password = await bcrypt.hash(body.password, 10);
+  const newUser = await Users.create({
+    name: body.name,
+    username: body.username,
+    password: encrypted_password,
+  }).catch(errorHandler);
+  if (newUser) {
+    res.json({
+      message: "user created",
+      user: {
+        token: req.token,
+        refreshToken: req.refreshToken,
+        username: body.username,
+      },
+    });
+  } else {
+    res.json({ message: "could not create user" });
+  }
+});
+
+router.post("/login", genJWT, async (req, res) => {
+  const body = req.body;
+  const user = await Users.findAll({
+    where: { username: body.username },
+  }).catch(errorHandler);
+  if (user) {
+    const decoded = await bcrypt.compare(body.password, user[0].password);
+    if (decoded) {
+      res.json({
+        user: {
+          token: req.token,
+          refreshToken: req.refreshToken,
+          username: body.username,
+        },
+      });
+    } else {
+      res.json({ message: "wrong password" });
+    }
+  } else {
+    res.json({ message: "no user found" });
+  }
+});
+
+router.post("/logout", deleteJWT);
 
 module.exports = router;
