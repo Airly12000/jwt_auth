4e5b5366142a63159df4ddd85a8497fe78a5d122
Airly12000 <littlefrancis28@gmail.com>
2024-01-12 02:18:32 +0000

    update

--- routes/Messages.js
+++ routes/Messages.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const Messages = require("../models/Messages");
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.json({ message: "Hello Message" });
+router.get("/", async (req, res) => {
+  const messages = await Messages.findAll({
+    where: { username: req.body.username },
+  });
+  res.json({ messages });
 });
 
 module.exports = router;
