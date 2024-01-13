18a61cd9fddfc5b295239edd42ac573f8a7292b3
Airly12000 <littlefrancis28@gmail.com>
2024-01-13 00:26:33 +0000

    messages routes

--- routes/Messages.js
+++ routes/Messages.js
@@ -3,11 +3,26 @@ const Messages = require("../models/Messages");
 
 const router = express.Router();
 
-router.get("/", async (req, res) => {
+const errorHandler = (error) => console.log(`Error: ${error}`);
+
+router.post("/", async (req, res) => {
   const messages = await Messages.findAll({
     where: { username: req.body.username },
-  });
+    attributes: ["message"],
+  }).catch(errorHandler);
   res.json({ messages });
+});
+
+router.post("/post", async (req, res) => {
+  const newMessage = await Messages.create({
+    message: req.body.message,
+    username: req.body.username,
+  }).catch(errorHandler);
+  if (newMessage) {
+    res.json({ message: "message created" });
+  } else {
+    res.json({ message: "failed to create message" });
+  }
 });
 
 module.exports = router;
